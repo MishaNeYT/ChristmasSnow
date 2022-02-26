0e4f9ccf73c881f3e1324aa537e84b42a749c9f3
MishaNeYT <mishapavlov987@gmail.com>
2022-02-26 15:56:38 +0900

    Оптимизация еее, спасибо папаша)

--- src/main/java/ru/mishaneyt/snow/Main.java
+++ src/main/java/ru/mishaneyt/snow/Main.java
@@ -7,15 +7,24 @@ import ru.mishaneyt.snow.utils.Utils;
 
 public class Main extends JavaPlugin {
     public static Main instance;
-
     public static Main getInstance() {
         return instance;
     }
+
+    public int Count;
+    public int RadiusX;
+    public int RadiusY;
+    public int RadiusZ;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         instance = this;
+
+        Count = Main.getInstance().getConfig().getInt("SnowToSpawn.Count");
+        RadiusX = Main.getInstance().getConfig().getInt("SnowToSpawn.RadiusX");
+        RadiusY = Main.getInstance().getConfig().getInt("SnowToSpawn.RadiusY");
+        RadiusZ = Main.getInstance().getConfig().getInt("SnowToSpawn.RadiusZ");
 
         getCommand("csnow").setExecutor(new Commands());
 
--- src/main/java/ru/mishaneyt/snow/utils/SnowUpdates.java
+++ src/main/java/ru/mishaneyt/snow/utils/SnowUpdates.java
@@ -16,11 +16,8 @@ public class SnowUpdates {
                 for (Player player : Bukkit.getServer().getOnlinePlayers())
                     if (Main.getInstance().getConfig().getBoolean("Settings.SnowToSpawn")) {
                         if (Main.getInstance().getConfig().getStringList("Settings.EnableWorld").contains(player.getLocation().getWorld().getName())) {
-                            player.spawnParticle(Particle.FIREWORKS_SPARK, player.getLocation(),
-                                    Main.getInstance().getConfig().getInt("SnowToSpawn.Count"),
-                                    Main.getInstance().getConfig().getInt("SnowToSpawn.RadiusX"),
-                                    Main.getInstance().getConfig().getInt("SnowToSpawn.RadiusY"),
-                                    Main.getInstance().getConfig().getInt("SnowToSpawn.RadiusZ"), 0.0);
+                            player.spawnParticle(Particle.FIREWORKS_SPARK, player.getLocation(), Main.getInstance().Count,
+                                    Main.getInstance().RadiusX, Main.getInstance().RadiusY, Main.getInstance().RadiusZ, 0.0);
                         }
                     }
             }
